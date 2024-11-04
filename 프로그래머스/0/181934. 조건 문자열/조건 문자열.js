eea941220c4111d9ff30d3570c75f6ec041cb1b8
devoil <devdevoildev@gmail.com>
2024-11-04 14:43:28 +0900

    [level 0] Title: 조건 문자열, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
@@ -1,18 +1,14 @@
 function solution(ineq, eq, n, m) {
     var answer = 0;
     
-    if(ineq === '>') {
-        if(eq === '=') {
-            n >= m ? answer = 1 : answer = 0;
-        } else {
-            n > m ? answer = 1 : answer = 0;
-        }
-    } else {
-        if(eq === '=') {
-            n <= m ? answer = 1 : answer = 0;
-        } else {
-            n < m ? answer = 1 : answer = 0;
-        }
+    if(ineq == ">" && eq == "="){
+        answer = n >= m ? 1 : 0;
+    }else if(ineq == "<" && eq == "="){
+        answer = n <= m ? 1 : 0;
+    }else if(ineq == ">" && eq == "!"){
+        answer = n > m ? 1 : 0;
+    }else{
+        answer = n < m ? 1 : 0;
     }
     return answer;
 }
