bcd3bc2ec1e873c068f2764c6706dd94322caa5f
devoil <devdevoildev@gmail.com>
2024-12-04 17:25:06 +0900

    ✨ Feat: [didact] step 4: Fibers

--- react-didact/script.js
+++ react-didact/script.js
@@ -22,7 +22,7 @@ function createTextElement(text) {
   }
 }
 
-function render(element, container) {
+function createDom(fiber) {
   const dom =
     element.type == "TEXT_ELEMENT"
       ? document.createTextNode("")
@@ -35,15 +35,22 @@ function render(element, container) {
       dom[name] = element.props[name]
     })
 
-  element.props.children.forEach(child =>
-    render(child, dom)
-  )
+  return dom
+}
 
-  container.appendChild(dom)
+// render 함수에서 fiber 트리의 루트에 nextUnitOfWork 함수 설정
+function render(element, container) {
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+  }
 }
 
 let nextUnitOfWork = null
 
+// 준비를 마친 브라우저가 workLoop를 호출하면, 루트에서부터 작업 시작
 function workLoop(deadline) {
   let shouldYield = false
   while (nextUnitOfWork && !shouldYield) {
@@ -56,6 +63,55 @@ function workLoop(deadline) {
 }
 
 requestIdleCallback(workLoop)
+
+// 새로운 노드 생성, DOM에 추가
+function performUnitOfWork(fiber) {
+  if(!fiber.dom) {
+    fiber.dom = createDom(fiber)
+  }
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom)
+  }
+
+  // 각각의 자식들마다 새로운 fiber 생성
+  const elements = fiber.props.children
+  let index = 0
+  let prevSibling = null
+
+  while (index < elements.length) {
+    const element = elements[index]
+
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    }
+
+    // 첫 번째 자식인지 아닌지에 따라 자식 혹은 형제 자매로서 fiber 트리에 추가
+    if (index === 0) {
+      fiber.child = newFiber
+    } else {
+      prevSibling.sibling = newFiber
+    }
+
+    prevSibling = newFiber
+    index++
+  }
+  // 탐색작업 (탐색 순서: 자식 -> 형제자매 -> 부모의 형제자매 순서)
+  if (fiber.child) {
+    return fiber.child
+  }
+  let nextFiber = fiber
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling
+    }
+    nextFiber = nextFiber.parent
+  }
+}
+
+
 
 function performUnitOfWork(nextUnitOfWork) {
   // TODO
