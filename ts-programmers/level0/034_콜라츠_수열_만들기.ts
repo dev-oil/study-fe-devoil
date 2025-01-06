c3c91c9f852ae511e66cc991e4bd17d350db9a21
devoil <devdevoildev@gmail.com>
2025-01-06 13:46:31 +0900

    ♻️ Refactor: [프로그래머스][level 0] 034

--- ts-programmers/level0/034_콜라츠_수열_만들기.ts
+++ ts-programmers/level0/034_콜라츠_수열_만들기.ts
@@ -1,17 +1,17 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/181919
 
 function solution(n: number, arr: number[] = []): number[] {
-  arr.push(n);
+  // push -> 배열 자체를 수정하기 때문에 ... 부작용 위험!
 
   if (n === 1) {
-    return arr; 
+    return [...arr, n]; // ... 로 새로운 배열 리턴
   }
 
   if (n % 2 === 0) {
-    return solution(n / 2, arr); 
+    return solution(n / 2, [...arr, n]); 
   }
 
-  return solution(3 * n + 1, arr);
+  return solution(3 * n + 1, [...arr, n]);
 }
 
 // test
